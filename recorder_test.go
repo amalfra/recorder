d540bc6a8b9d876fe7c6f050a8e796ca6bca626b
Amal Francis <amalfra@gmail.com>
2017-08-10 13:57:06 +0530

    add blackbox tests

---
+++ recorder_test.go
@@ -0,0 +1,49 @@
+package recorder_test
+
+import (
+	"fmt"
+	"os"
+	"recorder"
+	"strings"
+	"testing"
+)
+
+func TestStdout(t *testing.T) {
+	stdoutText := `This should be recorded.
+	second line that should be recorded~~~
+	end output`
+	stderrText := "this goes to stderr"
+	stdoutRecorder := new(recorder.Stdout)
+	fmt.Println("before recording")
+	err := stdoutRecorder.Start()
+	if err != nil {
+		t.Fatalf("Error initializing recorder: %s", err)
+	}
+	fmt.Println(stdoutText)
+	fmt.Fprintf(os.Stderr, "%s", stderrText)
+	stdoutRecorder.Stop()
+	recordedOutput := strings.TrimSpace(stdoutRecorder.GetOutput())
+	if stdoutText != recordedOutput {
+		t.Fatalf("Wrong output recorded: %s", recordedOutput)
+	}
+}
+
+func TestStderr(t *testing.T) {
+	stderrText := `This should be recorded.
+	second line that should be recorded~~~
+	end output`
+	stdoutText := "this goes to stdout"
+	stderrRecorder := new(recorder.Stderr)
+	fmt.Println("before recording")
+	err := stderrRecorder.Start()
+	if err != nil {
+		t.Fatalf("Error initializing recorder: %s", err)
+	}
+	fmt.Println(stdoutText)
+	fmt.Fprintf(os.Stderr, "%s", stderrText)
+	stderrRecorder.Stop()
+	recordedOutput := strings.TrimSpace(stderrRecorder.GetOutput())
+	if stderrText != recordedOutput {
+		t.Fatalf("Wrong output recorded: %s", recordedOutput)
+	}
+}
