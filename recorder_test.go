5249618b916a57c7f0a3a19285038d0a38bbbd82
Amal Francis <amalfra@gmail.com>
2017-08-11 14:14:49 +0530

    add advanced testcases

--- recorder_test.go
+++ recorder_test.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/amalfra/recorder"
 )
+
+var wg sync.WaitGroup
 
 func TestStdout(t *testing.T) {
 	stdoutText := `This should be recorded.
@@ -47,4 +51,36 @@ func TestStderr(t *testing.T) {
 	if stderrText != recordedOutput {
 		t.Fatalf("Wrong output recorded: %s", recordedOutput)
 	}
+}
+
+func TestStdoutGoRoutine(t *testing.T) {
+	stdoutText := `This should be recorded.
+	second line that should be recorded~~~
+	end output`
+	stderrText := "this goes to stderr"
+	stdoutRecorder := new(recorder.Stdout)
+	fmt.Println("before recording")
+	err := stdoutRecorder.Start()
+	if err != nil {
+		t.Fatalf("Error initializing recorder: %s", err)
+	}
+	fmt.Println(stdoutText)
+	wg.Add(1)
+	go goRoutine()
+	wg.Wait()
+	fmt.Fprintf(os.Stderr, "%s", stderrText)
+	stdoutRecorder.Stop()
+	recordedOutput := strings.TrimSpace(stdoutRecorder.GetOutput())
+	stdoutText += "\nin goRoutine"
+	stdoutText += "\nout goRoutine"
+	if stdoutText != recordedOutput {
+		t.Fatalf("Wrong output recorded: %s", recordedOutput)
+	}
+}
+
+func goRoutine() {
+	defer wg.Done()
+	fmt.Println("in goRoutine")
+	time.Sleep(1000 * time.Millisecond)
+	fmt.Println("out goRoutine")
 }
